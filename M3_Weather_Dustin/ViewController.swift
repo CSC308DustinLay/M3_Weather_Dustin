e175967b0c433b332f4c83b0bc7a7ce011efd116
Dustin <redhotblood1010@gmail.com>
2026-03-26 12:10:45 -0400

    yes

--- M3_Weather_Dustin/ViewController.swift
+++ M3_Weather_Dustin/ViewController.swift
@@ -14,56 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet var background: UIView!
     @IBOutlet weak var messageLabel: UILabel!
     
-    func getWeatherImage(matching weather: String) -> UIImage?
-    {
-        switch weather
-        {
-            case "Sunny":
-                return UIImage(named: "sunny")
-            case "Cloudy":
-                return UIImage(named: "cloudy")
-            case "Raining":
-                return UIImage(named: "rainy")
-            case "Snowing":
-                return UIImage(named: "snowy")
-            default:
-                return UIImage(named: "weather")
-        }
-    }
-    
-    func getTemperature(matching temp: Int) -> String
-    {
-        switch temp
-        {
-            case ..<20:
-                return "Extremely Cold"
-            case 20...49:
-                return "Cold"
-            case 50...68:
-                return "Cool and mild"
-            case 69...85:
-                return "Hot"
-            case 86...:
-                return "Extremely Hot"
-            default :
-                return ""
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let weather = ["Sunny", "Snowy", "Rainy", "Cloudy"].randomElement() ?? "Weather"
+        let weather = [Weather.sunny, Weather.rainy, Weather.snowy, Weather.cloudy].randomElement() ?? .sunny
         let temperature = Int.random(in: 0...100)
         
         //Set Labels
-        statusLabel.text = weather
+        statusLabel.text = weather.rawValue
         temperatureLabel.text = "\(temperature)°F"
         
         //get Image
-        ImageView.image = getWeatherImage(matching: weather)
+        ImageView.image = weather.image
         //get Temperature
-        messageLabel.text = getTemperature(matching: temperature)
+        messageLabel.text = weather.getTemperature(matching: temperature)
     }
 }
 
--- M3_Weather_Dustin/model/Weather.swift
+++ M3_Weather_Dustin/model/Weather.swift
@@ -5,4 +5,43 @@
 //  Created by Heather Bishop on 3/26/26.
 //
 
-import Foundation
+import UIKit
+
+enum Weather: String
+{
+    case sunny = "Sunny", snowy = "Snowy", rainy = "Rainy", cloudy = "Cloudy"
+    
+    var image: UIImage?
+    {
+        switch self
+        {
+        case .sunny:
+                return UIImage(named: "sunny")
+        case .cloudy:
+                return UIImage(named: "cloudy")
+        case .rainy:
+                return UIImage(named: "rainy")
+        case .snowy:
+                return UIImage(named: "snowy")
+        }
+    }
+    
+    func getTemperature(matching temp: Int) -> String
+    {
+        switch temp
+        {
+            case ..<20:
+                return "Extremely Cold"
+            case 20...49:
+                return "Cold"
+            case 50...68:
+                return "Cool and mild"
+            case 69...85:
+                return "Hot"
+            case 86...:
+                return "Extremely Hot"
+            default :
+                return ""
+        }
+    }
+}
